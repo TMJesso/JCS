0307f84be27ff1f4b1400685c8f82d2fcf6ab757
Theral Jessop <Jess_Hort_Farms@mail.com>
2018-07-15 15:30:37 -0400

    Created a template to use on each file

---
+++ TEMPLATE.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'includes/initialize.php';
+
+$subtitle = "TEMPLATE";
+?>
+<?php include_layout_template('jcs_login_header.php'); // use jcs_header.php to include menus  ?>
+<!-- html content goes below here -->
+<div class="grid-container">
+	<div class="grid-x gird-padding-x">
+		<div class="large-12 medium-12 cell"> <!-- this will produce a 12 column grid and should not ever be more than 12 -->
+		<!-- remove all this text before adding your html -->
+		This is test and will show as html
+		you can put <h1>heading 1</h1> here
+		
+		<p>Or you can put paragraphs here also</p>
+		
+		
+		</div>
+	</div>
+</div>
+
+
+<!-- html content goes above here -->
+<?php include_layout_template('jcs_footer.php'); ?>
+
+
