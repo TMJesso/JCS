333867cd4432658d4dab749e29387a1e40e963e0
Theral Jessop <Jess_Hort_Farms@mail.com>
2018-07-15 14:57:23 -0400

    Added Resume index

--- resume/public/admin/index.php
+++ resume/public/admin/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../../../includes/initialize.php';
-$menu_type = Menu_Type::get_by_type("CLAD", 9);
-$subtitle = "Welcome to the Change Log and Application Development log";
+$menu_type = Menu_Type::get_by_type("R&amp;eacute;sum&amp;eacute;", 9);
+$subtitle = "Welcome to the Resume";
 ?>
 <?php include_layout_template('jcs_header.php'); ?>
 You have reached the admin index for CLAD!
